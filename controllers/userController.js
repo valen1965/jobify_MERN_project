4059331cc9cbc36845a7a1c3a6142a8572389a1d
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2024-03-12 13:42:15 +0200

    fix update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ import Job from '../models/jobModel.js';
 import 'express-async-errors';
 import { StatusCodes } from 'http-status-codes';
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findById({ _id: req.user.userId });
@@ -20,8 +20,8 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
